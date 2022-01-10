bcecc601807b704230a79d41fef92568af1de048
Refael Whyte <r.whyte@chronoptics.com>
2022-01-10 16:18:49 +1300

    Added python example

--- README.md
+++ README.md
@@ -3,7 +3,21 @@
 ![Scheme](doc/chronoptics.png)
 
 ## Description
-Example of running code on the Chronoptics Kea depth camera. [Chronoptics](www.chronoptics.com). Chronoptics designs and develops depth sensing solutions, for more information on Kea please contact Chronoptics, or email me r.whyte@chronoptics.com 
+Example of running Python and C++ on the Chronoptics Kea depth camera. [Chronoptics](www.chronoptics.com). Chronoptics designs and develops depth sensing solutions, for more information on Kea please contact Chronoptics, or email me r.whyte@chronoptics.com 
 
 The Kea Yocto layer provides the recipe to build this project and add it to the rootfs. Otherwise generate the SDK and build. 
+
+## Build 
+To build, you need to source the Yocto SDK to find the dependencies and cross-compile for aarch64
+> mkdir build && cd build \ 
+> cmake ../ \  
+> make 
+
+To run on the camera 
+> DISPLAY=:0 kea_opencv_viewer 
+
+## Python 
+Copy the display_camera_stream.py file over to the camera. You will have to update the PYTHONPATH to include the Chronoptics library on the camera. On the camera
+> export PYTHONPATH=/usr/lib/python \ 
+> DISPLAY=:0 python3 display_camera_stream.py 
 
---
+++ python/display_camera_stream.py
@@ -0,0 +1,60 @@
+"""
+Example of using the EmbeddedKeaCamera class to access intensity data from the 
+camera using Python. 
+
+For issues and support please contact me, Refael Whyte: r.whyte@chronoptics.com 
+"""
+
+# %%
+# This is a hacky way to get on camera python working, as OpenMP linking isn't working
+import numpy as np
+import matplotlib.pyplot as plt
+import chronoptics.tof as tof
+import ctypes
+from ctypes.util import find_library
+tmp_lib = find_library("gomp")
+ctypes.CDLL(tmp_lib, mode=ctypes.RTLD_GLOBAL)
+
+
+def handle_close(evt):
+    cam.stop()
+    return
+
+
+proc = tof.ProcessingConfig()
+
+user = tof.UserConfig()
+
+cam = tof.EmbeddedKeaCamera(proc)
+config = cam.getCameraConfig()
+
+tof.selectStreams(cam, [tof.FrameType.INTENSITY])
+
+fig = plt.figure()
+fig.canvas.mpl_connect("close_event", handle_close)
+
+roi = config.getRoi(0)
+nrows = roi.getImgRows()
+ncols = roi.getImgCols()
+
+plt.subplot(111)
+rimg = plt.imshow(
+    np.zeros((nrows, ncols), dtype=np.uint8),
+    cmap="gray",
+    vmin=0,
+    vmax=255,
+    interpolation="none",
+)
+plt.title("Radial")
+
+cam.start()
+while cam.isStreaming():
+    frames = cam.getFrames()
+
+    rimg.set_data(np.asarray(frames[0]))
+    plt.draw()
+
+    plt.show(block=False)
+    plt.pause(0.1)
+
+# %%
